c8a54cc1eeab4dc1a5dbce5352458be3c04b6071
Rene Kubitzek <rene.kubitzek@diva-e.com>
2023-09-13 17:34:37 +0200

    inital commit for Backend View

---
+++ src/Orm/Zed/ChatMessage/Persistence/PyzChatMessage.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Orm\Zed\ChatMessage\Persistence;
+
+use Orm\Zed\ChatMessage\Persistence\Base\PyzChatMessage as BasePyzChatMessage;
+
+/**
+ * Skeleton subclass for representing a row from the 'pyz_chat_message' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
+class PyzChatMessage extends BasePyzChatMessage
+{
+
+}
